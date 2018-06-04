6f4ce99e5a82e8e8705c3a45583c15b919ee4eb9
calvin419 <36018855+calvin419@users.noreply.github.com>
2018-06-04 15:58:16 -0700

    Added my unit test for fighting stage

---
+++ src/tests/FightingStageTests.java
@@ -0,0 +1,12 @@
+//Author: Connor Alvin
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestFightingStage.class, TestFightingStage2.class })
+public class FightingStageTests {
+
+}
---
+++ src/tests/TestFightingStage2.java
@@ -0,0 +1,64 @@
+//Author: Connor Alvin
+
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.fighterz.main.Fighter;
+import com.fighterz.main.FighterFalessi;
+import com.fighterz.main.FightingStage;
+import com.fighterz.main.Window;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestFightingStage2 {
+	
+	public static class AsNonApp extends Application {
+		@Override
+		public void start(Stage primaryStage) throws Exception {
+		}
+	}
+
+	
+	@BeforeClass
+	public static void initJFX() throws InterruptedException {
+		Thread t = new Thread("JavaFX Init Thread") {
+			public void run() {
+				try {
+					Application.launch(AsNonApp.class, new String[0]);
+				} catch (Exception e) {
+					// Don't launch another application if one is already running
+				}	
+			}
+		};
+		t.setDaemon(true);
+		t.start();
+		Thread.sleep(500);
+	}
+	
+	@Test
+	public void testFightingStageSetup2() throws InterruptedException {
+		try {
+			Window.main(null);
+		}
+		catch(Exception e) {
+			
+		}
+		
+		FightingStage fs = Window.getGame().getFightingStage();
+		
+		Fighter f = new FighterFalessi("right");
+		
+		fs.setFighterRight(f);
+		
+		fs.nullFighters();
+		
+		assertNull(fs.getFighterRight());
+		
+	}
+
+}
