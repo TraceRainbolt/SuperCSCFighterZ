6f88df11353742e7f5725259c1d236378485cea1
connsteele <35544211+connsteele@users.noreply.github.com>
2018-06-04 17:43:50 -0700

    Anotha I said anotha one

--- src/tests/ConnorS_TestingSuite.java
+++ src/tests/ConnorS_TestingSuite.java
@@ -9,6 +9,6 @@ import org.junit.runners.Suite.SuiteClasses;
 //test 2x units tests first then 2x integration tests
 @RunWith(Suite.class)
 @SuiteClasses({ TestFighters3.class, TestFighters4.class, TestIntegration.class, TestIntegration2.class }) 
-public class MoreFighterTests {
+public class ConnorS_TestingSuite {
 
 }
